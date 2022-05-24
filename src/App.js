56cfb189353bdb7ca4430516ef35057af8d5196b
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-05-24 22:20:49 +0200

    Refactor App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React from 'react';
 import './App.css';
 import Calculator from './components/calculator';
 
-function App() {
-  return (
-    (<Calculator />)
-  );
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  render() {
+    return (
+      (<Calculator />)
+    );
+  }
 }
 
 export default App;
