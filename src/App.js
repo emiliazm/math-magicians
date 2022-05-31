158b3f1899b057b05587b956e555e787c34f6ae1
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-05-31 23:15:43 +0200

    Fix nav and router. Add Navigate

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   Routes,
   Route,
   Link,
+  Navigate,
 } from 'react-router-dom';
 import Home from './components/home';
 import Quote from './components/quote';
@@ -15,16 +16,21 @@ class App extends React.Component {
   render() {
     return (
       <Router>
-        <h1>Math Magician</h1>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/calculator">Calculator</Link>
-          <Link to="/quote">Quote</Link>
-        </nav>
+        <div className="header-container">
+          <h1>Math Magician</h1>
+          <nav>
+            <Link to="/">Home</Link>
+            <span> | </span>
+            <Link to="/calculator">Calculator</Link>
+            <span> | </span>
+            <Link to="/quote">Quote</Link>
+          </nav>
+        </div>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/calculator" element={<Calculator previous="" operand="" current="0" solved={false} />} />
           <Route path="/quote" element={<Quote />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Router>
     );
