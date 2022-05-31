1a733ff1678d2d12e45a4f5f7efc7e3c3fa98da2
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-05-31 22:02:55 +0200

    Add Routers and Links to Add.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from 'react-router-dom';
+import Home from './components/home';
+import Quote from './components/quote';
 import Calculator from './components/calculator';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends React.Component {
   render() {
     return (
-      (<Calculator previous="" operand="" current="0" solved={false} />)
+      <Router>
+        <h1>Math Magician</h1>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/calculator">Calculator</Link>
+          <Link to="/quote">Quote</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator previous="" operand="" current="0" solved={false} />} />
+          <Route path="/quote" element={<Quote />} />
+        </Routes>
+      </Router>
     );
   }
 }
