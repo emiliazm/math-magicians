e1cc3e2ab1a60b515ea51981b8f9ce61e92cacaa
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-06-01 00:44:13 +0200

    Fix router

--- src/App.js
+++ src/App.js
@@ -22,15 +22,14 @@ class App extends React.Component {
             <Link to="/">Home</Link>
             <span> | </span>
             <Link to="/calculator">Calculator</Link>
-            <span> | </span>
             <Link to="/quote">Quote</Link>
           </nav>
         </div>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/calculator" element={<Calculator previous="" operand="" current="0" solved={false} />} />
           <Route path="/quote" element={<Quote />} />
-          <Route path="*" element={<Navigate to="/" />} />
+          <Route path="*" element={<Navigate to="/home" />} />
         </Routes>
       </Router>
     );
