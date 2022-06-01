3a7f47e5090fbb22968baa4b582499544011803a
arturo <ahpareja@gmail.com>
2022-06-01 17:20:21 -0500

    Add test to calculator

--- src/tests/links.test.js
+++ src/tests/links.test.js
@@ -1,5 +1,6 @@
 import { render, fireEvent, screen } from '@testing-library/react';
 import App from '../App';
+import Calculator from '../components/calculator';
 
 test('links work properly', () => {
   const { container } = render(<App url="/" />);
@@ -17,4 +18,14 @@ test('links work properly', () => {
   const { container } = render(<App url="/quote" />);
   fireEvent.click(screen.getByText('Quote'));
   expect(container.getElementsByClassName('quote')[0]).toHaveTextContent('Mathematics is not about numbers');
+});
+
+test('When a user clicks in the Calculator link, the Calculator page should appear', () => {
+  render(<Calculator />);
+  fireEvent.click(screen.queryByText('6'));
+  fireEvent.click(screen.queryByText('x'));
+  fireEvent.click(screen.queryByText('8'));
+  fireEvent.click(screen.queryByText('='));
+
+  expect(screen.queryByText('48')).not.toBeNull();
 });
